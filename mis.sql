b9bed6478548e0ec41dc6c468a96391d607bfb42
Prem Chand Saini <premchandsaini779@gmail.com>
2017-11-13 00:50:52 +0530

    dabase upadte

--- mis.sql
+++ mis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2017 at 11:37 AM
+-- Generation Time: Nov 12, 2017 at 08:09 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -47,7 +47,34 @@ CREATE TABLE `booking` (
 INSERT INTO `booking` (`booking_id`, `customer_id`, `room_id`, `booking_date`, `check_in`, `check_out`, `total_price`, `remaining_price`, `payment_status`) VALUES
 (1, 1, 1, '2017-11-09 05:30:37', '10-11-2017', '14-11-2017', 5000, 0, 1),
 (2, 2, 2, '2017-11-12 09:15:13', '13-11-2017', '17-11-2017', 7500, 0, 1),
-(3, 3, 1, '2017-11-12 09:18:13', '13-11-2017', '15-11-2017', 3000, 0, 0);
+(3, 3, 1, '2017-11-12 09:18:13', '13-11-2017', '15-11-2017', 3000, 0, 1),
+(4, 4, 2, '2017-11-12 10:46:18', '14-11-2017', '17-11-2017', 6000, 4000, 0),
+(5, 5, 1, '2017-11-12 10:58:34', '13-11-2017', '17-11-2017', 5000, 5000, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaint`
+--
+
+CREATE TABLE `complaint` (
+  `id` int(11) NOT NULL,
+  `complainant_name` varchar(100) NOT NULL,
+  `complaint_type` varchar(100) NOT NULL,
+  `complaint` varchar(200) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `resolve_status` tinyint(1) NOT NULL,
+  `resolve_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `budget` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `complaint`
+--
+
+INSERT INTO `complaint` (`id`, `complainant_name`, `complaint_type`, `complaint`, `created_at`, `resolve_status`, `resolve_date`, `budget`) VALUES
+(1, 'Prem Chand Saini', 'Light', 'Light', '2017-11-12 18:29:18', 1, '2017-11-12 18:59:30', 1000),
+(2, 'Ajit Kumar Jain', 'Water', 'Water', '2017-11-12 18:34:21', 0, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -72,7 +99,9 @@ CREATE TABLE `customer` (
 INSERT INTO `customer` (`customer_id`, `customer_name`, `contact_no`, `email`, `id_card_type_id`, `id_card_no`, `address`) VALUES
 (1, 'Ajit Kumar Jain', 9808788721, 'premchandsaini779@gmail.com', 1, 12345341212, 'asdasdasd'),
 (2, 'Ajit Kumar Jain', 9887554425, 'premchandsaini779@gmail.com', 1, 422510099122, 'Ahmedabad'),
-(3, 'Prem Chand Saini', 9887665544, '201452019@iiitvadodara.ac.in', 3, 12345341212, 'Gandhinagar');
+(3, 'Prem Chand Saini', 9887665544, '201452019@iiitvadodara.ac.in', 3, 12345341212, 'Gandhinagar'),
+(4, 'Prem Chand Saini', 9887553324, 'premchandsaini779@gmail.com', 1, 0, 'asdasdasdsd'),
+(5, 'Prem Chand Saini', 9887554422, 'premchandsaini779@gmail.com', 3, 0, 'asdad');
 
 -- --------------------------------------------------------
 
@@ -122,16 +151,17 @@ CREATE TABLE `room` (
   `room_no` varchar(10) NOT NULL,
   `status` tinyint(1) DEFAULT NULL,
   `check_in_status` tinyint(1) NOT NULL,
-  `check_out_status` tinyint(1) NOT NULL
+  `check_out_status` tinyint(1) NOT NULL,
+  `deleteStatus` tinyint(1) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `room`
 --
 
-INSERT INTO `room` (`room_id`, `room_type_id`, `room_no`, `status`, `check_in_status`, `check_out_status`) VALUES
-(1, 1, '601', 1, 0, 1),
-(2, 2, '602', NULL, 0, 1);
+INSERT INTO `room` (`room_id`, `room_type_id`, `room_no`, `status`, `check_in_status`, `check_out_status`, `deleteStatus`) VALUES
+(1, 1, '601', NULL, 0, 1, 1),
+(2, 2, '602', 1, 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -185,31 +215,25 @@ INSERT INTO `shift` (`shift_id`, `shift`, `shift_timing`) VALUES
 --
 
 CREATE TABLE `staff` (
-  `emp_id` int(10) NOT NULL,
+  `emp_id` int(11) NOT NULL,
   `emp_name` varchar(100) NOT NULL,
-  `staff_type_id` int(10) NOT NULL,
-  `shift_id` int(10) NOT NULL,
-  `id_card_type` int(10) NOT NULL,
-  `id_card_no` bigint(50) NOT NULL,
+  `staff_type_id` int(11) NOT NULL,
+  `shift_id` int(11) NOT NULL,
+  `id_card_type` int(11) NOT NULL,
+  `id_card_no` bigint(20) NOT NULL,
   `address` varchar(100) NOT NULL,
-  `contact_no` bigint(10) NOT NULL,
+  `contact_no` bigint(20) NOT NULL,
+  `salary` bigint(20) NOT NULL,
   `joining_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `salary` int(10) NOT NULL
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `staff`
 --
 
-INSERT INTO `staff` (`emp_id`, `emp_name`, `staff_type_id`, `shift_id`, `id_card_type`, `id_card_no`, `address`, `contact_no`, `joining_date`, `salary`) VALUES
-(3, 'Deepak Goyal', 1, 4, 1, 201452058, '806 room', 88888888, '0000-00-00 00:00:00', 320000),
-(6, 'sharadsharad patelpatel', 2, 3, 3, 666, 'pdpu', 0, '0000-00-00 00:00:00', 123),
-(8, 'deepak..goyal', 1, 2, 4, 123, 'gec', 0, '0000-00-00 00:00:00', 2000),
-(13, 'sdfghjk   fghj', 2, 3, 1, 99, 'kjk', 0, '0000-00-00 00:00:00', 99999),
-(14, 'sdfghjk   fghj', 2, 3, 1, 99, 'kjk', 0, '0000-00-00 00:00:00', 99999),
-(15, 'andrew Ng', 1, 1, 1, 321654981, 'Gandhinagar', 989898989, '0000-00-00 00:00:00', 5000),
-(17, 'Anjali   Kumari', 1, 2, 1, 123456789, 'jgfasdghacfskgj', 123456789, '0000-00-00 00:00:00', 123456),
-(18, 'Prem Chand', 1, 1, 1, 422510099122, 'Ap #897-1459 Quam Avenue', 9887554425, '0000-00-00 00:00:00', 10000);
+INSERT INTO `staff` (`emp_id`, `emp_name`, `staff_type_id`, `shift_id`, `id_card_type`, `id_card_no`, `address`, `contact_no`, `salary`, `joining_date`, `updated_at`) VALUES
+(2, 'Prem Chand', 1, 3, 1, 422510099122, 'Ap #897-1459 Quam Avenue', 9887554425, 10000, '0000-00-00 00:00:00', '2017-11-12 17:31:39');
 
 -- --------------------------------------------------------
 
@@ -252,7 +276,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `username`, `email`, `password`, `created_at`) VALUES
-(1, 'Prem Chand', 'pcsaini', 'premchandsaini779@gmail.com', '37a7e2cf07ff08f43e5cd844e567a23f', '2017-11-12 10:04:03');
+(1, 'Prem Chand', 'pcsaini', 'premchandsaini779@gmail.com', '37a7e2cf07ff08f43e5cd844e567a23f', '2017-11-12 10:04:03'),
+(2, 'Ajit Kumar', '201452064', '201452064@iiitvadoda.ac.in', 'fb1e75090b96d2e0abec0ec980c2f392', '2017-11-12 12:49:22'),
+(3, 'Sharad Patel', '201452030', '201452030@iiitvadodara.ac.in', 'd0a512f262ed34abed0c45cefe08c429', '2017-11-12 12:49:22');
 
 --
 -- Indexes for dumped tables
@@ -265,6 +291,12 @@ ALTER TABLE `booking`
   ADD PRIMARY KEY (`booking_id`),
   ADD KEY `customer_id` (`customer_id`),
   ADD KEY `room_id` (`room_id`);
+
+--
+-- Indexes for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `customer`
@@ -311,7 +343,7 @@ ALTER TABLE `shift`
 --
 ALTER TABLE `staff`
   ADD PRIMARY KEY (`emp_id`),
-  ADD KEY `identity_card_type` (`id_card_type`),
+  ADD KEY `id_card_type` (`id_card_type`),
   ADD KEY `shift_id` (`shift_id`),
   ADD KEY `staff_type_id` (`staff_type_id`);
 
@@ -335,13 +367,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `booking_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `booking_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `complaint`
+--
+ALTER TABLE `complaint`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `emp_history`
@@ -377,7 +415,7 @@ ALTER TABLE `shift`
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `emp_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `staff_type`
@@ -389,7 +427,7 @@ ALTER TABLE `staff_type`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
