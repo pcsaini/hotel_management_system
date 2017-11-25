b05511b99b2aef91601c71dcc69073e70fa51f0e
Prem Chand Saini <premchandsaini779@gmail.com>
2017-11-25 18:39:18 +0530

    SQL File update

--- mis.sql
+++ mis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2017 at 08:09 PM
+-- Generation Time: Nov 25, 2017 at 02:08 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -45,11 +45,11 @@ CREATE TABLE `booking` (
 --
 
 INSERT INTO `booking` (`booking_id`, `customer_id`, `room_id`, `booking_date`, `check_in`, `check_out`, `total_price`, `remaining_price`, `payment_status`) VALUES
-(1, 1, 1, '2017-11-09 05:30:37', '10-11-2017', '14-11-2017', 5000, 0, 1),
-(2, 2, 2, '2017-11-12 09:15:13', '13-11-2017', '17-11-2017', 7500, 0, 1),
-(3, 3, 1, '2017-11-12 09:18:13', '13-11-2017', '15-11-2017', 3000, 0, 1),
-(4, 4, 2, '2017-11-12 10:46:18', '14-11-2017', '17-11-2017', 6000, 4000, 0),
-(5, 5, 1, '2017-11-12 10:58:34', '13-11-2017', '17-11-2017', 5000, 5000, 0);
+(1, 1, 5, '2017-11-13 05:45:17', '13-11-2017', '15-11-2017', 3000, 3000, 0),
+(2, 2, 2, '2017-11-13 05:46:04', '13-11-2017', '16-11-2017', 6000, 0, 1),
+(3, 3, 2, '2017-11-11 06:49:19', '11-11-2017', '14-11-2017', 6000, 6000, 0),
+(4, 4, 7, '2017-11-09 06:50:24', '11-11-2017', '15-11-2017', 10000, 10000, 0),
+(5, 5, 13, '2017-11-17 06:59:10', '17-11-2017', '20-11-2017', 12000, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -73,8 +73,10 @@ CREATE TABLE `complaint` (
 --
 
 INSERT INTO `complaint` (`id`, `complainant_name`, `complaint_type`, `complaint`, `created_at`, `resolve_status`, `resolve_date`, `budget`) VALUES
-(1, 'Prem Chand Saini', 'Light', 'Light', '2017-11-12 18:29:18', 1, '2017-11-12 18:59:30', 1000),
-(2, 'Ajit Kumar Jain', 'Water', 'Water', '2017-11-12 18:34:21', 0, NULL, NULL);
+(1, 'Ravi Kumar Patel', 'Room Windows', 'Room Windows', '2017-11-13 06:51:24', 1, '2017-11-15 06:51:58', 1000),
+(2, 'Uday Kumar', 'Light', 'Light', '2017-11-13 06:51:44', 1, '2017-11-18 07:06:02', 10000),
+(3, 'Rajeev Kumar Singh', 'Water', 'Water Crisis on Bathroom', '2017-11-17 07:01:17', 1, '2017-11-18 07:01:52', 1500),
+(4, 'Aniket Raj', 'Water', 'Water in boooooo', '2017-11-18 07:05:49', 0, '2017-11-18 07:05:49', NULL);
 
 -- --------------------------------------------------------
 
@@ -88,7 +90,7 @@ CREATE TABLE `customer` (
   `contact_no` bigint(20) NOT NULL,
   `email` varchar(100) NOT NULL,
   `id_card_type_id` int(10) NOT NULL,
-  `id_card_no` bigint(20) NOT NULL,
+  `id_card_no` varchar(20) NOT NULL,
   `address` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -97,11 +99,11 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`customer_id`, `customer_name`, `contact_no`, `email`, `id_card_type_id`, `id_card_no`, `address`) VALUES
-(1, 'Ajit Kumar Jain', 9808788721, 'premchandsaini779@gmail.com', 1, 12345341212, 'asdasdasd'),
-(2, 'Ajit Kumar Jain', 9887554425, 'premchandsaini779@gmail.com', 1, 422510099122, 'Ahmedabad'),
-(3, 'Prem Chand Saini', 9887665544, '201452019@iiitvadodara.ac.in', 3, 12345341212, 'Gandhinagar'),
-(4, 'Prem Chand Saini', 9887553324, 'premchandsaini779@gmail.com', 1, 0, 'asdasdasdsd'),
-(5, 'Prem Chand Saini', 9887554422, 'premchandsaini779@gmail.com', 3, 0, 'asdad');
+(1, 'Ravi Kumar Patel', 8775566122, '201452019@iiitvadodara.ac.in', 1, '422510099122', 'Ahmedabad'),
+(2, 'Aniket Raj', 9887778878, '201452042@iiitvadodara.ac.in', 2, '422510099122', 'Ahmedabad'),
+(3, 'Sunny Sunny', 9887665441, 'ajitkumarjain123@gmail.com', 1, '422510099122', 'Jaipur'),
+(4, 'Uday  Kumar', 9888755544, '201452018@iiitvadoara.ac.in', 3, '0', 'Gujarat'),
+(5, 'Prem Chand Saini', 9887554425, 'premchandsaini779@gmail.com', 1, '422510099122', 'Ahmedabad');
 
 -- --------------------------------------------------------
 
@@ -114,9 +116,34 @@ CREATE TABLE `emp_history` (
   `emp_id` int(11) NOT NULL,
   `shift_id` int(11) NOT NULL,
   `from_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `to_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `to_date` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `emp_history`
+--
+
+INSERT INTO `emp_history` (`id`, `emp_id`, `shift_id`, `from_date`, `to_date`, `created_at`) VALUES
+(1, 1, 1, '2017-11-13 05:39:06', '2017-11-15 02:22:26', '2017-11-13 05:39:06'),
+(2, 2, 3, '2017-11-13 05:39:39', '2017-11-15 02:22:43', '2017-11-13 05:39:39'),
+(3, 3, 1, '2017-11-13 05:40:18', '2017-11-15 02:22:49', '2017-11-13 05:40:18'),
+(4, 4, 1, '2017-11-13 05:40:56', '2017-11-15 02:22:35', '2017-11-13 05:40:56'),
+(5, 5, 1, '2017-11-13 05:41:31', NULL, '2017-11-13 05:41:31'),
+(6, 6, 3, '2017-11-13 05:42:03', NULL, '2017-11-13 05:42:03'),
+(7, 7, 4, '2017-11-13 05:42:35', '2017-11-18 02:35:02', '2017-11-13 05:42:35'),
+(8, 8, 3, '2017-11-13 05:43:13', '2017-11-18 02:32:26', '2017-11-13 05:43:13'),
+(9, 9, 2, '2017-11-13 05:43:49', NULL, '2017-11-13 05:43:49'),
+(10, 10, 1, '2017-11-13 06:30:45', '2017-11-18 02:34:28', '2017-11-13 06:30:45'),
+(11, 1, 2, '2017-11-15 06:52:26', '2017-11-17 02:23:05', '2017-11-15 06:52:26'),
+(12, 4, 3, '2017-11-15 06:52:35', NULL, '2017-11-15 06:52:35'),
+(13, 2, 3, '2017-11-15 06:52:43', NULL, '2017-11-15 06:52:43'),
+(14, 3, 3, '2017-11-15 06:52:49', NULL, '2017-11-15 06:52:49'),
+(15, 1, 3, '2017-11-17 06:53:05', NULL, '2017-11-17 06:53:05'),
+(16, 8, 1, '2017-11-18 07:02:26', NULL, '2017-11-18 07:02:26'),
+(17, 11, 2, '2017-11-18 07:04:03', NULL, '2017-11-18 07:04:03'),
+(18, 10, 2, '2017-11-18 07:04:28', NULL, '2017-11-18 07:04:28'),
+(19, 7, 2, '2017-11-18 07:05:02', NULL, '2017-11-18 07:05:02');
 
 -- --------------------------------------------------------
 
@@ -160,8 +187,19 @@ CREATE TABLE `room` (
 --
 
 INSERT INTO `room` (`room_id`, `room_type_id`, `room_no`, `status`, `check_in_status`, `check_out_status`, `deleteStatus`) VALUES
-(1, 1, '601', NULL, 0, 1, 1),
-(2, 2, '602', 1, 1, 1, 0);
+(1, 2, 'A-101', NULL, 0, 0, 1),
+(2, 2, 'A-102', 1, 0, 1, 0),
+(3, 3, 'A-103', NULL, 0, 0, 0),
+(4, 4, 'A-104', NULL, 0, 0, 0),
+(5, 1, 'B-101', 1, 0, 0, 0),
+(6, 2, 'B-102', NULL, 0, 0, 0),
+(7, 3, 'B-103', 1, 0, 0, 0),
+(8, 4, 'B-104', NULL, 0, 0, 0),
+(9, 1, 'C-101', NULL, 0, 0, 0),
+(10, 2, 'C-102', NULL, 0, 0, 0),
+(11, 3, 'C-103', NULL, 0, 0, 0),
+(12, 4, 'C-104', NULL, 0, 0, 0),
+(13, 4, 'D-101', NULL, 0, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -220,7 +258,7 @@ CREATE TABLE `staff` (
   `staff_type_id` int(11) NOT NULL,
   `shift_id` int(11) NOT NULL,
   `id_card_type` int(11) NOT NULL,
-  `id_card_no` bigint(20) NOT NULL,
+  `id_card_no` varchar(20) NOT NULL,
   `address` varchar(100) NOT NULL,
   `contact_no` bigint(20) NOT NULL,
   `salary` bigint(20) NOT NULL,
@@ -233,7 +271,17 @@ CREATE TABLE `staff` (
 --
 
 INSERT INTO `staff` (`emp_id`, `emp_name`, `staff_type_id`, `shift_id`, `id_card_type`, `id_card_no`, `address`, `contact_no`, `salary`, `joining_date`, `updated_at`) VALUES
-(2, 'Prem Chand', 1, 3, 1, 422510099122, 'Ap #897-1459 Quam Avenue', 9887554425, 10000, '0000-00-00 00:00:00', '2017-11-12 17:31:39');
+(1, 'Prem Chand Saini', 1, 3, 1, '422510099122', 'Jaipur, Rajasthan', 987554425, 100000, '2017-11-13 05:39:06', '2017-11-17 06:53:05'),
+(2, 'Anjali Kumari', 3, 3, 1, '422510099122', 'Jaipur, Rajasthan', 976543211, 10000, '0000-00-00 00:00:00', '2017-11-13 05:44:10'),
+(3, 'Ajit Kumar Jain', 2, 3, 1, '422510099122', 'Ap #897-1459 Quam Avenue', 976543111, 10000, '2017-11-13 05:40:18', '2017-11-15 06:52:49'),
+(4, 'Deepak Goyal', 2, 3, 2, '0', 'Ap #897-1459 Quam Avenue', 987654321, 100000, '2017-11-13 05:40:55', '2017-11-15 06:52:35'),
+(5, 'Sharad Patel', 4, 1, 1, '12345341212', 'Ap #897-1459 Quam Avenue', 9876543212, 10000, '2017-11-13 05:41:31', '2017-11-13 05:41:31'),
+(6, 'Vijay Anand', 3, 3, 3, '0', 'Ap #897-1459 Quam Avenue', 1234567890, 100000, '2017-11-13 05:42:03', '2017-11-13 05:42:03'),
+(7, 'Lalit  Kumar', 2, 2, 1, '422510099122', 'Ap #897-1459 Quam Avenue', 12322332231, 100000, '2017-11-13 05:42:35', '2017-11-18 07:05:02'),
+(8, 'Rajeev Kumar Singh', 1, 1, 4, '0', 'Ap #897-1459 Quam Avenue', 976543456, 10000, '2017-11-13 05:43:13', '2017-11-18 07:02:26'),
+(9, 'Himanshu  Soni', 3, 2, 4, '0', 'Ap #897-1459 Quam Avenue', 98765432123, 100000, '2017-11-13 05:43:49', '2017-11-13 05:43:49'),
+(10, 'Sandeep Merutha', 5, 2, 1, '422510099122', 'Ap #897-1459 Quam Avenue', 9887665534, 1000, '2017-11-13 06:30:45', '2017-11-18 07:04:28'),
+(11, 'Aniket Raj', 3, 2, 1, '0', 'Ap #897-1459 Quam Avenue', 9887556655, 10000, '2017-11-18 07:04:03', '2017-11-18 07:04:03');
 
 -- --------------------------------------------------------
 
@@ -254,7 +302,8 @@ INSERT INTO `staff_type` (`staff_type_id`, `staff_type`) VALUES
 (1, 'Manager'),
 (2, 'Cleaning'),
 (3, 'Reception'),
-(4, 'Cook');
+(4, 'Cook'),
+(5, 'Waiter');
 
 -- --------------------------------------------------------
 
@@ -373,7 +422,7 @@ ALTER TABLE `booking`
 -- AUTO_INCREMENT for table `complaint`
 --
 ALTER TABLE `complaint`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `customer`
@@ -385,7 +434,7 @@ ALTER TABLE `customer`
 -- AUTO_INCREMENT for table `emp_history`
 --
 ALTER TABLE `emp_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `id_card_type`
@@ -397,7 +446,7 @@ ALTER TABLE `id_card_type`
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `room_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `room_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `room_type`
@@ -415,13 +464,13 @@ ALTER TABLE `shift`
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `staff_type`
 --
 ALTER TABLE `staff_type`
-  MODIFY `staff_type_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `staff_type_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user`
